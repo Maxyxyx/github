4be89bf4bba080ca4cbf7e675aa54ddbc1cc3b30
m.kroschewski <m.kroschewski@hgs-singen.online>
2021-11-24 13:08:40 +0100

    added

--- app/src/main/java/com/example/github/MainActivity.java
+++ app/src/main/java/com/example/github/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.github;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView tvX;
+    private TextView tvY;
+    private TextView tvZ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Teast
+        //textview-objekte referenzieren
+        tvX = findViewById(R.id.textViewX);
+        tvY = findViewById(R.id.textViewY);
+        tvZ = findViewById(R.id.textViewZ);
+
+        //Sensormanager
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //accleration sensor
+        Sensor sensorAcc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //register sensor listener
+        sm.registerListener((SensorEventListener) this, sensorAcc, sm.SENSOR_DELAY_NORMAL);
+    }
+
+
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        tvX.setText("X:"+event.values[0]);
+        tvY.setText("Y:"+event.values[1]);
+        tvZ.setText("Z:"+event.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
---
+++ app/src/main/java/com/example/github/MainActivity2.java
@@ -0,0 +1,7 @@
+package com.example.github;
+
+import android.hardware.Sensor;
+
+public interface MainActivity2 {
+    void onAccuracychanged(Sensor sensor, int accuracy);
+}
